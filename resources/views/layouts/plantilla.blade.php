5c1a8a27c2211e340c6eb39f59e806314d2f4203
Benjamin <bmonkda@gmail.com>
2023-04-24 09:59:24 -0400

    comiteando para crear una nueva rama

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -1,24 +1,37 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
+    
     <meta charset="utf-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
-    <title>Sales Admin | CORK - Multipurpose Bootstrap Dashboard Template </title>
+    
+    {{-- <title>Sales Admin | CORK - Multipurpose Bootstrap Dashboard Template </title> --}}
+    <title>{{ config('app.name', 'INCIDENCIAS') }} - @yield('title')</title>
+    
     {{-- <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico"/> --}}
-    <link rel="icon" type="image/x-icon" href="{{ asset('../layouts/plantilla/assets/img/favicon.ico') }}"/>
+    <link rel="icon" type="image/x-icon" href={{ url('../layouts/plantilla/assets/img/favicon.ico') }}/>
+    {{-- <link rel="icon" type="image/x-icon" href="{{ asset('../layouts/plantilla/assets/img/favicon.ico') }}"/> --}}
+    
     {{-- <link href="assets/css/loader.css" rel="stylesheet" type="text/css" /> --}}
     <link href="{{ asset('../layouts/plantilla/assets/css/loader.css') }}" rel="stylesheet" type="text/css" />
+    
     {{-- <script src="assets/js/loader.js"></script> --}}
     <script src="{{ asset('../layouts/plantilla/assets/js/loader.js') }}"></script>
+    
+    
     <!-- BEGIN GLOBAL MANDATORY STYLES -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
+    
     {{-- <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" /> --}}
     <link href="{{ asset('../layouts/plantilla/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+    
     {{-- <link href="assets/css/plugins.css" rel="stylesheet" type="text/css" /> --}}
     <link href="{{ asset('../layouts/plantilla/assets/css/plugins.css') }}" rel="stylesheet" type="text/css" />
     <!-- END GLOBAL MANDATORY STYLES -->
 
+    
     <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM STYLES -->
     <link href="plugins/apex/apexcharts.css" rel="stylesheet" type="text/css">
     <link href="assets/css/dashboard/dash_1.css" rel="stylesheet" type="text/css" />
