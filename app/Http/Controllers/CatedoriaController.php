b1c426205db63c8a5ed74762b86dbcbaeb4badcd
JOSE <bmonkdareg@gmail.com>
2023-04-25 21:54:19 -0400

    Creación de los Modelos Categoria, Subcategoria, Emergencia, Statu con sus  migraciones, controladores con su recursos

---
+++ app/Http/Controllers/CatedoriaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Catedoria;
+use Illuminate\Http\Request;
+
+class CatedoriaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Catedoria  $catedoria
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Catedoria $catedoria)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Catedoria  $catedoria
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Catedoria $catedoria)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Catedoria  $catedoria
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Catedoria $catedoria)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Catedoria  $catedoria
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Catedoria $catedoria)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/SubCatedoriaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SubCatedoria;
+use Illuminate\Http\Request;
+
+class SubCatedoriaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\SubCatedoria  $subCatedoria
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SubCatedoria $subCatedoria)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\SubCatedoria  $subCatedoria
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SubCatedoria $subCatedoria)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\SubCatedoria  $subCatedoria
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SubCatedoria $subCatedoria)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\SubCatedoria  $subCatedoria
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SubCatedoria $subCatedoria)
+    {
+        //
+    }
+}
